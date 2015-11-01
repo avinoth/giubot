78e07dc9f00f39ad2530bcc903cb359292b0d7d2
Vinoth <a.vinothk@outlook.com>
2015-11-01 15:10:42 +0530

    Add API endpoint

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'httparty'
+require 'json'
+
+post '/gateway' do
+  message = params[:text].gsub(params[:trigger_word], '').strip
+
+  action, repo = message.split('_').map {|c| c.strip.downcase }
+  repo_url = "https://api.github.com/repos/#{repo}"
+
+  case action
+    when 'issues'
+      resp = HTTParty.get(repo_url)
+      resp = JSON.parse resp.body
+      respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
+  end
+end
+
+def respond_message message
+  content_type :json
+  {:text => message}.to_json
+end
